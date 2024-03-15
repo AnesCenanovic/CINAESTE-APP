7eb901af8025ac82f176cd6f4cecab691f919f9c
AnesCenanovic <127337745+AnesCenanovic@users.noreply.github.com>
2024-03-15 19:26:15 +0100

    constraint layout

---
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.lab1
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button = findViewById<Button>(R.id.button1);
+        button.setOnClickListener(){
+            showMessage();
+        }
+    }
+
+    private fun showMessage(){
+        val editText = findViewById<EditText>(R.id.editText1);
+        val textView = findViewById<TextView>(R.id.textView1);
+        val message = editText.text.toString();
+        Log.v("TESTLOG", message);
+        textView.text = message;
+    }
+}
