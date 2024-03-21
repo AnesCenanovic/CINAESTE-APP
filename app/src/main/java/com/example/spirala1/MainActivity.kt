50e86086a8d32f1da2c36b9bffb5048e4a9b7c4d
AnesCenanovic <127337745+AnesCenanovic@users.noreply.github.com>
2024-03-21 21:31:18 +0100

    cinesta project

---
+++ app/src/main/java/com/example/spirala1/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.spirala1
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var favoriteMovies: RecyclerView
+    private lateinit var favoriteMoviesAdapter: MovieListAdapter
+    private var favoriteMoviesList =  getFavoriteMovies()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        favoriteMovies = findViewById(R.id.favoriteMovies)
+        favoriteMovies.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        favoriteMoviesAdapter = MovieListAdapter(listOf())
+        favoriteMovies.adapter = favoriteMoviesAdapter
+        favoriteMoviesAdapter.updateMovies(favoriteMoviesList)
+    }
+}
+
+class MovieListAdapter(
+    private var movies: List<Movie>
+) : RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.layout, parent, false)
+        return MovieViewHolder(view)
+    }
+    override fun getItemCount(): Int = movies.size
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.movieTitle.text = movies[position].title;
+        val genreMatch: String = movies[position].genre
+        //Pronalazimo id drawable elementa na osnovu naziva zanra
+        val context: Context = holder.movieImage.context
+        var id: Int = context.resources
+            .getIdentifier(genreMatch, "drawable", context.packageName)
+        if (id==0) id=context.resources
+            .getIdentifier("picture1", "drawable", context.packageName)
+        holder.movieImage.setImageResource(id)
+    }
+    fun updateMovies(movies: List<Movie>) {
+        this.movies = movies
+        notifyDataSetChanged()
+    }
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val movieImage: ImageView = itemView.findViewById(R.id.movieImage)
+        val movieTitle: TextView = itemView.findViewById(R.id.movieTitle)
+    }
+}
+
+data class Movie(
+    val id: Long,
+    val title: String,
+    val overview: String,
+    val releaseDate: String,
+    val homepage: String,
+    val genre: String
+)
+
+fun getFavoriteMovies(): List<Movie> {
+    return listOf(
+        Movie(1,"Pride and prejudice",
+            "Sparks fly when spirited Elizabeth Bennet meets single, rich, and proud Mr. Darcy. But Mr. Darcy reluctantly finds himself falling in love with a woman beneath his class. Can each overcome their own pride and prejudice?",
+            "16.02.2005.","https://www.imdb.com/title/tt0414387/",
+            "drama"),
+        Movie(2,"The Shawshank Redemption",
+            "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+            "14.10.1994.","https://www.imdb.com/title/tt0111161/",
+            "drama"),
+        Movie(3,"Inception",
+            "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+            "16.07.2010.","https://www.imdb.com/title/tt1375666/",
+            "sci-fi"),
+        Movie(4,"The Dark Knight",
+            "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+            "18.07.2008.","https://www.imdb.com/title/tt0468569/",
+            "action")
+    )
+}
+fun getRecentMovies(): List<Movie> {
+    return listOf(
+        Movie(1,"Furiosa: A Mad Max Saga",
+            "The origin story of renegade warrior Furiosa before her encounter and teamup with Mad Max.",
+            "24.05.2024.","https://www.imdb.com/title/tt12037194",
+            "action"),
+        Movie(2,"Spider-Man: No Way Home",
+            "With Spider-Man's identity now revealed, Peter Parker asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.",
+            "17.12.2021.","https://www.imdb.com/title/tt10872600/",
+            "action"),
+        Movie(3,"Dune",
+            "Feature adaptation of Frank Herbert's science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and most vital element in the galaxy.",
+            "21.10.2021.","https://www.imdb.com/title/tt1160419/",
+            "sci-fi"),
+        Movie(4,"Eternals",
+            "The saga of the Eternals, a race of immortal beings who lived on Earth and shaped its history and civilizations.",
+            "05.11.2021.","https://www.imdb.com/title/tt9032400/",
+            "sci-fi")
+    )
+}
