15d93118bf588321ac4e75bb029d922e3a0d0ddd
AnesCenanovic <127337745+AnesCenanovic@users.noreply.github.com>
2024-04-01 22:09:12 +0200

    Teme i boje, skaliranje slika

--- app/src/main/java/com/example/spirala1/MainActivity.kt
+++ app/src/main/java/com/example/spirala1/MainActivity.kt
@@ -18,10 +18,14 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
     private lateinit var favoriteMovies: RecyclerView
     private lateinit var favoriteMoviesAdapter: MovieListAdapter
+    private lateinit var recentMovies: RecyclerView
+    private lateinit var recentMoviesAdapter: MovieListAdapterRecent
     private var favoriteMoviesList =  getFavoriteMovies()
+    private var recentMoviesList =  getRecentMovies()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         favoriteMovies = findViewById(R.id.favoriteMovies)
         favoriteMovies.layoutManager = LinearLayoutManager(
@@ -32,6 +36,16 @@ class MainActivity : AppCompatActivity() {
         favoriteMoviesAdapter = MovieListAdapter(arrayListOf()) { movie -> showMovieDetails(movie) }
         favoriteMovies.adapter = favoriteMoviesAdapter
         favoriteMoviesAdapter.updateMovies(favoriteMoviesList)
+
+        recentMovies = findViewById(R.id.recentMovies)
+        recentMovies.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        recentMoviesAdapter = MovieListAdapterRecent(arrayListOf()) { movie -> showMovieDetails(movie) }
+        recentMovies.adapter = recentMoviesAdapter
+        recentMoviesAdapter.updateMovies(recentMoviesList)
 
 
     }
@@ -48,6 +62,39 @@ class MovieListAdapter(
     private var movies: List<Movie>,
     private val onItemClicked: (movie:Movie) -> Unit
 ) : RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.layout, parent, false)
+        return MovieViewHolder(view)
+    }
+    override fun getItemCount(): Int = movies.size
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.movieTitle.text = movies[position].title;
+        val genreMatch: String = movies[position].genre
+        //Pronalazimo id drawable elementa na osnovu naziva zanra
+        val context: Context = holder.movieImage.context
+        var id: Int = context.resources
+            .getIdentifier(genreMatch, "drawable", context.packageName)
+        if (id==0) id=context.resources
+            .getIdentifier("picture1", "drawable", context.packageName)
+        holder.movieImage.setImageResource(id)
+        holder.itemView.setOnClickListener{ onItemClicked(movies[position]) }
+    }
+    fun updateMovies(movies: List<Movie>) {
+        this.movies = movies
+        notifyDataSetChanged()
+    }
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val movieImage: ImageView = itemView.findViewById(R.id.movieImage)
+        val movieTitle: TextView = itemView.findViewById(R.id.movieTitle)
+    }
+}
+
+class MovieListAdapterRecent(
+    private var movies: List<Movie>,
+    private val onItemClicked: (movie:Movie) -> Unit
+) : RecyclerView.Adapter<MovieListAdapterRecent.MovieViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val view = LayoutInflater
             .from(parent.context)
@@ -99,7 +146,7 @@ fun getFavoriteMovies(): List<Movie> {
         Movie(3,"Inception",
             "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
             "16.07.2010.","https://www.imdb.com/title/tt1375666/",
-            "sci-fi"),
+            "scifi"),
         Movie(4,"The Dark Knight",
             "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
             "18.07.2008.","https://www.imdb.com/title/tt0468569/",
@@ -119,10 +166,10 @@ fun getRecentMovies(): List<Movie> {
         Movie(3,"Dune",
             "Feature adaptation of Frank Herbert's science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and most vital element in the galaxy.",
             "21.10.2021.","https://www.imdb.com/title/tt1160419/",
-            "sci-fi"),
+            "scifi"),
         Movie(4,"Eternals",
             "The saga of the Eternals, a race of immortal beings who lived on Earth and shaped its history and civilizations.",
             "05.11.2021.","https://www.imdb.com/title/tt9032400/",
-            "sci-fi")
+            "scifi")
     )
 }
